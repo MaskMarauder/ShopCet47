f67b7c02ad4196875f4b45c73a141b678d43d303
Goncalo Afonso <G.valerio@sapo.pt>
2020-09-04 23:01:58 +0100

    Create Database

---
+++ ShopCet47.Web/Data/Entities/product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopCet47.Web.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+
+
+        public string Name { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public string Price { get; set; }
+
+
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+
+        [Display(Name = "Last Purcharse")]
+        public DateTime LastPurchase { get; set; }
+
+
+        [Display(Name = "LastSale")]
+        public DateTime LastSale { get; set; }
+
+
+        [Display(Name = "IsAvailable")]
+        public string IsAvailable { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)] 
+        public string Stock { get; set; }
+    }
+}
---
+++ ShopCet47.Web/Migrations/20200904214252_InitialDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopCet47.Web.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    LastPurchase = table.Column<DateTime>(nullable: false),
+                    LastSale = table.Column<DateTime>(nullable: false),
+                    IsAvailable = table.Column<string>(nullable: true),
+                    Stock = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
